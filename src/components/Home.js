2dc97a47eaf200dcf8e8c91cd44dd0b9fcf99531
Jelenka Clausen <hallo@wortgewitzt.de>
2022-10-21 15:50:52 +0200

    centered home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home(props) {
 return(
  
         <div className="app" id="app">
-        <div className="app-wrapper grid">
+        <div className="app-wrapper grid" style={{minHeight: 300}}>
             <button id="new-zyklus" onClick={props.handleOldCyclesBtn}>Old Cycles</button>
             <button id="new-zyklus" style={{marginTop: 15}} onClick={props.handleNewCycleBtn}>Start New Cycle</button>
         </div>
