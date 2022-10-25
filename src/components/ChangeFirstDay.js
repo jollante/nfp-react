0692a8bc9ed444b1cba1716424ead3bbcd5aa2b3
Jelenka Clausen <hallo@wortgewitzt.de>
2022-10-25 11:37:52 +0200

    styling changes in change first day

--- src/components/ChangeFirstDay.js
+++ src/components/ChangeFirstDay.js
@@ -11,10 +11,10 @@ export default function ChangeFirstDay(props) {
 
     return (
         <div id='app' className="app">
-            <button id="home-btn" onClick={props.handleBackBtn}>Back</button>
-            <h1 style={{margin: 10}}>Change First Day Of Period</h1>
+            <button style={{padding: '0.4em 0.6em'}} id="home-btn" onClick={props.handleBackBtn}>Back</button>
+            <h1 style={{margin: 10, textAlign: 'center', color: '#b14f8c'}}>Change First Day</h1>
             <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <input type='date' name='new-first-day' onChange={getFirstDay} value={dayjs(newFirstDay).format('YYYY-MM-DD')}/>
+                <input style={{marginTop: 10, marginBottom: 0}} type='date' name='new-first-day' onChange={getFirstDay} value={dayjs(newFirstDay).format('YYYY-MM-DD')}/>
                 <button type='button' onClick={() => props.changeFirstDay(newFirstDay)}>Save Changes</button>
             </form>
         </div>
