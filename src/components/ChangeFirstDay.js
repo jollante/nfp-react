bd12f44aaadb76c68d9b9fe9088149762aae97c9
Jelenka Clausen <hallo@wortgewitzt.de>
2022-10-25 12:06:52 +0200

    style changes in changeFirstDay

--- src/components/ChangeFirstDay.js
+++ src/components/ChangeFirstDay.js
@@ -1,21 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import dayjs from 'dayjs';
 
 export default function ChangeFirstDay(props) {
 
-    const [ newFirstDay, setNewFirstDay ] = useState(props.firstDay)
-    
-    const getFirstDay = ({target}) => {
+    const [newFirstDay, setNewFirstDay] = useState(props.firstDay)
+
+    const getFirstDay = ({ target }) => {
         setNewFirstDay(target.value)
     }
 
     return (
         <div id='app' className="app">
-            <button style={{padding: '0.4em 0.6em'}} id="home-btn" onClick={props.handleBackBtn}>Back</button>
-            <h1 style={{margin: 10, textAlign: 'center', color: '#b14f8c'}}>Change First Day</h1>
-            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <input style={{marginTop: 10, marginBottom: 0}} type='date' name='new-first-day' onChange={getFirstDay} value={dayjs(newFirstDay).format('YYYY-MM-DD')}/>
-                <button type='button' onClick={() => props.changeFirstDay(newFirstDay)}>Save Changes</button>
+            <div style={{display: 'grid', gridTemplateColumns:'1fr 3fr', marginBottom: 20 }}>
+                <button style={{ padding: '0.4em 0.6em', marginTop: 0 }} id="home-btn" onClick={props.handleBackBtn}>Back</button>
+                <h1 style={{ margin: 10, textAlign: 'center', color: '#b14f8c' }}>Change First Day<span style={{fontWeight: 'normal', fontSize: 14, display: 'block'}}> Of Period</span></h1>
+            </div>
+
+            <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', border: '2px solid #42ada3', borderRadius: 5, padding: '40px 0px'  }}>
+                <input style={{ marginBottom: 0 }} type='date' name='new-first-day' onChange={getFirstDay} value={dayjs(newFirstDay).format('YYYY-MM-DD')} />
+                <button type='button' onClick={() => props.changeFirstDay(newFirstDay)}>Save</button>
             </form>
         </div>
     )
